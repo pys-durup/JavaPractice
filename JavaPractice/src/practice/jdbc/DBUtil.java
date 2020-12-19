237fac7e9ff5fe906350b133affd283ac30a2834
durup <enenfnq@gmail.com>
2020-12-20 00:13:21 +0900

    DButil.java Connection 전용 클래스

---
+++ JavaPractice/src/practice/jdbc/DBUtil.java
@@ -0,0 +1,16 @@
+package practice.jdbc;
+
+import java.sql.Connection;
+
+
+public class DBUtil {
+
+	private static Connection conn;
+	
+	public static Connection open() {
+		
+		String url = "";
+		
+		return conn;
+	}
+}
