c1831941ebd40c2651f5b9eae198d9e1aa376918
durup <enenfnq@gmail.com>
2021-04-04 23:51:47 +0900

    Baekjoon1920 정답

--- JavaPractice/src/practice/algorithm/Baekjoon1920.java
+++ JavaPractice/src/practice/algorithm/Baekjoon1920.java
@@ -3,6 +3,8 @@ package practice.algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Baekjoon1920 {
 
@@ -24,36 +26,38 @@ public class Baekjoon1920 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
 		int high = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		String setNumbers = br.readLine();
-		
-		int numberCount = Integer.parseInt(br.readLine()); // 반복 횟수.
-		
-		String getNumbers = br.readLine();
-		
-		String[] setArray = setNumbers.split(" ");
-		//System.out.println(setArray.toString());
-		//System.out.println(setArray[0]);
-		
-		String[] numArray = getNumbers.split(" ");
-		
-		for (int i=0; i<setArray.length-1; i++) {
-			for (int j=i+1; j<setArray.length; j++) {
-				if (Integer.parseInt(setArray[j]) < Integer.parseInt(setArray[i])) {
-					String temp = setArray[j];
-					setArray[j] = setArray[i];
-					setArray[i] = temp;
-				}
-			}
+		int[] setArray = new int[high];
+		for(int i=0; i<high; i++) {
+			setArray[i] = Integer.parseInt(st.nextToken());
 		}
 		
-//		for (int i=0; i<setArray.length; i++) {
-//			System.out.println(setArray[i]);
+		
+		int numberCount = Integer.parseInt(br.readLine()); // 반복 횟수.
+		st = new StringTokenizer(br.readLine());
+		int[] numArray = new int[numberCount];
+		for(int i=0; i<numberCount; i++) {
+			numArray[i] = Integer.parseInt(st.nextToken());
+		}
+		
+	
+		Arrays.sort(setArray);
+//		for (int i=0; i<setArray.length-1; i++) {
+//			for (int j=i+1; j<setArray.length; j++) {
+//				if (setArray[j] < setArray[i]) {
+//					int temp = setArray[j];
+//					setArray[j] = setArray[i];
+//					setArray[i] = temp;
+//				}
+//			}
 //		}
 		
 		
-		for (int i=0; i<numArray.length; i++) {
+		for (int i=0; i<numberCount; i++) {
 			System.out.println(binarySearch(setArray, numArray[i]));
 		}
 		
@@ -63,18 +67,18 @@ public class Baekjoon1920 {
 		
 	}
 
-	private static int binarySearch(String[] setArray, String getNum) {
+	private static int binarySearch(int[] setArray, int getNum) {
 		int high = setArray.length - 1;
 		int low = 0;
 		
 		while (low <= high) {
 			int middle = (low + high)/2;
 			
-			if(setArray[middle].equals(getNum)) {
+			if(setArray[middle] == getNum) {
 				return 1;
 			}
 			
-			if(Integer.parseInt(setArray[middle]) < Integer.parseInt(getNum)) {
+			if(setArray[middle] < getNum) {
 				low = middle + 1;
 			} else {
 				high = middle - 1;
